f652b0dadbaf347c00e1fb33d6489abceafae73a
SashaGeng <ozaliznickij@gmail.com>
2023-03-01 16:37:58 +0200

    lab0 done

---
+++ lab0/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.Date;
+import static java.lang.Math.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Main prog = new Main();
+        prog.run();
+    }
+    private double calcS (double a, double b, double x){
+        return (pow(x,3)*pow(tan(pow(x+b,2)),2)+(a/sqrt(x+b)));
+    }
+    private double calcQ (double a, double b, double x){
+        return ((b*pow(x,2)-a)/(exp(a*x)-1));
+    }
+
+    private void print(double x, double S, double a, double b, double Q) {
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        System.out.println("x="+x);
+        System.out.println("------------------------------");
+        System.out.println("S="+S);
+        System.out.println("Q="+Q);
+    }
+    private void run() {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Введіть число а=");
+        double a = s.nextDouble();
+        System.out.print("Введіть число b=");
+        double b = s.nextDouble();
+        System.out.print("Введіть число x=");
+        double x = s.nextDouble();
+        double S=calcS(a,b,x);
+        double Q=calcQ(a,b,x);
+        print (x,S,a,b,Q);
+
+        Date date = new Date();
+        printDate(date);
+    }
+
+    private void printDate(Date date){
+        System.out.println("------------------------------");
+        System.out.printf("%1$td %1$tb %1$tY\n", date);
+    }
+}
